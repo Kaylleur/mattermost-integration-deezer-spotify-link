0807941a4076e99889f5060c199de7f3d6b01b28
tbrunet <tbrunet@itrust.fr>
2017-09-12 09:48:55 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+ # mattermost-integration-deezer-spotify-link
+ 
+ Detect and convert Spotify or Deezer links into each others   
+ 
+ ####Already in development...
+ 
+ To start
+ ```
+ npm install
+ npm start
+
+```
---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

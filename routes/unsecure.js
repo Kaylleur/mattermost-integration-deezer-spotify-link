4f5442ed40a19192955d23c5ea04ae2cfe0da433
Thomas Brunet <dev@thomas-brunet.fr>
2020-10-20 23:05:27 +0200

    first try

--- routes/unsecure.js
+++ routes/unsecure.js
@@ -2,14 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 /**
- * @api {get} /user/:id Request User information
- * @apiName GetUser
- * @apiGroup User
+ * @api {get} /unsecure/test Request Test
+ * @apiName GetTest
+ * @apiGroup Test
  *
- * @apiParam {Number} id Users unique ID.
- *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} lastname  Lastname of the User.
+ * @apiSuccess {String} message Ok.
  */
 router.get('/test', (req, res) => {
   res.send( { message: 'ok' } );
